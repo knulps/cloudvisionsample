d706f29ef6f6a9962de03f4cadb612018c5502d6
Knulps <dhlee@thecheat.co.kr>
2017-11-17 21:49:45 +0900

    Added function to Upload Firebase Storage

--- app/src/main/java/momu/cloudvisionsample/MainActivity.java
+++ app/src/main/java/momu/cloudvisionsample/MainActivity.java
@@ -3,9 +3,11 @@ package momu.cloudvisionsample;
 import android.Manifest;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,22 +15,37 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.TedPermission;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import gun0912.tedbottompicker.TedBottomPicker;
+
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView imgPic;
     TextView txtCenter;
+    UploadTask uploadTask;
+
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    private static final String API_KEY = BuildConfig.API_KEY;
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         imgPic = (ImageView) findViewById(R.id.img_picture);
         txtCenter = (TextView) findViewById(R.id.txt_center);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -81,18 +98,54 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    /**
+     * pick image from gallary
+     */
     void pickImage() {
         TedBottomPicker tedBottomPicker = new TedBottomPicker.Builder(MainActivity.this)
                 .setOnImageSelectedListener(new TedBottomPicker.OnImageSelectedListener() {
                     @Override
                     public void onImageSelected(Uri uri) {
                         // here is selected uri
-
                         imgPic.setImageURI(uri);
+                        uploadToFirebase(uri);
                     }
                 })
                 .create();
 
         tedBottomPicker.show(getSupportFragmentManager());
+    }
+
+    /**
+     * Upload to firebase Storage
+     *
+     * @param uri Uri to Upload
+     */
+    void uploadToFirebase(Uri uri) {
+        // Create a storage reference from our app
+        StorageReference storageRef = storage.getReference();
+
+        long time = System.currentTimeMillis();
+        SimpleDateFormat dayTime = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+        String currentTime = dayTime.format(new Date(time));
+
+        // Create a reference to 'images/mountains.jpg'
+        StorageReference imageRef = storageRef.child("images/location_" + currentTime + ".jpg");
+        uploadTask = imageRef.putFile(uri);
+
+        // Register observers to listen for when the download is done or if it fails
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                Log.e(TAG, "downloadUrl : " + downloadUrl);
+            }
+        });
     }
 }
